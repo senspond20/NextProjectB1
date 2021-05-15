6264d909f4bfa3ae58426f974603710ab6b0d277
senspond20 <senspond@gmail.com>
2021-05-15 20:51:53 +0900

    no css test

--- src/components/editor/Codemirror.tsx
+++ src/components/editor/Codemirror.tsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 // import {IUnControlledCodeMirror, UnControlled as CodeMirror} from 'react-codemirror2'
 import CodeMirror, { IReactCodemirror } from '@uiw/react-codemirror';
-import 'codemirror/addon/display/autorefresh';
-import 'codemirror/addon/comment/comment';
-import 'codemirror/addon/edit/matchbrackets';
+// import 'codemirror/addon/display/autorefresh';
+// import 'codemirror/addon/comment/comment';
+// import 'codemirror/addon/edit/matchbrackets';
 
 // import 'codemirror/mode/javascript/javascript'
-import 'codemirror/lib/codemirror.css'
+// import 'codemirror/lib/codemirror.css'
 
 // import 'codemirror/theme/default.css'
-import 'codemirror/theme/material.css'
+// import 'codemirror/theme/material.css'
 // import 'codemirror/theme/base16-dark.css'
-import 'codemirror/theme/ayu-dark.css'
-import 'codemirror/theme/monokai.css';
-import 'codemirror/keymap/sublime';
-const theme = 'monokai'; // material
+// import 'codemirror/theme/ayu-dark.css'
+// import 'codemirror/theme/monokai.css';
+// import 'codemirror/keymap/sublime';
+// const theme = 'monokai'; // material
 
 
 const CodeView = (props: 
@@ -31,7 +31,7 @@ const CodeView = (props:
         {...props}
         value={props.value}
         options={{
-            theme: theme, 
+            theme: 'default', 
             mode: 'jsx',
             tabSize: 3,
             keyMap: 'sublime',
